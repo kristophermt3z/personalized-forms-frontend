0333af75ed794bfaafd0b0594670eb54d0479c2c
kristopher <salvador.martinez.ksmg@gmail.com>
2024-12-24 01:12:24 -0600

    animation

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -32,11 +32,9 @@ const ParticlesBackground: React.FC<ParticlesBackgroundProps> = ({
       fpsLimit: 120,
       interactivity: {
         events: {
-          onClick: { enable: true, mode: "push" },
           onHover: { enable: true, mode: "repulse" },
         },
         modes: {
-          push: { quantity: 4 },
           repulse: { distance: 200, duration: 0.4 },
         },
       },
