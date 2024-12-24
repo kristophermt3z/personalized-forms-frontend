2429533c810c953f93617aa45408818e8db60381
kristopher <salvador.martinez.ksmg@gmail.com>
2024-12-23 19:13:51 -0600

    contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./styles/Contact.css";
 import Button from "../components/Button";
+import { ReactComponent as LinkedInIcon } from "../assets/linkedin.svg";
+import { ReactComponent as GitHubIcon } from "../assets/github.svg";
+import { ReactComponent as PortfolioIcon } from "../assets/portafolio.svg";
 
 const Contact: React.FC = () => {
   const navigate = useNavigate();
@@ -16,6 +19,29 @@ const Contact: React.FC = () => {
         <h1 className="title">Empower your creativity with dynamic forms.</h1>
         <Button onClick={handleLogin} label="Get Started" />
         <p className="footer">Created by Kristopher Martinez</p>
+        <div className="social-links">
+          <a
+            href="https://www.linkedin.com/in/kristophermt3z/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedInIcon className="icon" />
+          </a>
+          <a
+            href="https://github.com/kristophermt3z"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubIcon className="icon" />
+          </a>
+          <a
+            href="https://salvadormartin3z.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <PortfolioIcon className="icon" />
+          </a>
+        </div>
       </div>
     </div>
   );
