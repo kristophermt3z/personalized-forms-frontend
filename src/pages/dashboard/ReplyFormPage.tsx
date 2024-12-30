e982a39696cab871a8f2dff93f718fb6da70cdd4
kristopher <salvador.martinez.ksmg@gmail.com>
2024-12-29 22:18:25 -0600

    popup after reply a template

--- src/pages/dashboard/ReplyFormPage.tsx
+++ src/pages/dashboard/ReplyFormPage.tsx
@@ -1,27 +1,36 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import FormTemplate from "../../components/Forms/FormTemplate";
 import { submitReply } from "../../services/replyService";
-import './styles/Reply-ViewFormPage.css'
-
+import "./styles/Reply-ViewFormPage.css";
+import Popup from "../../components/Popup";
 
 const ReplyFormPage: React.FC = () => {
-  const { formId } = useParams<{ formId: string }>();
+  const navigate = useNavigate();
 
-  const handleSubmit = async (responses: { fieldId: string; answer: string }[]) => {
+  const { formId } = useParams<{ formId: string }>();
+  const [popupMessage, setPopupMessage] = useState<string | null>(null);
+
+  const handleSubmit = async (
+    responses: { fieldId: string; answer: string }[]
+  ) => {
     try {
       const token = localStorage.getItem("token") || "";
       await submitReply({ formId, responses }, token);
-      alert("Reply submitted successfully!");
+      setPopupMessage("Reply submitted successfully!");
+      setTimeout(() => navigate("/profile-forms"), 2000);
     } catch (error) {
       console.error("Error submitting reply:", error);
-      alert("Failed to submit reply. Please try again.");
+      setPopupMessage("Failed to submit reply. Please try again.");
     }
   };
 
   return (
     <div className="viewOrReplyFormTemplate">
       <FormTemplate formId={formId || ""} onSubmit={handleSubmit} />
+      {popupMessage && (
+        <Popup message={popupMessage} onClose={() => setPopupMessage(null)} />
+      )}
     </div>
   );
 };
